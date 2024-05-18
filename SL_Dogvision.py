987a330139ddfe44d15491a5f2f7922bb602f192
agorsu <155503066+agorsu@users.noreply.github.com>
2024-05-18 22:17:19 +1000

    Temp file error handling and layout update

--- SL_Dogvision.py
+++ SL_Dogvision.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from PIL import Image
+import tempfile, os
 
 from util import classify, load_model
 
@@ -11,17 +12,21 @@ st.title('Dog Vision')
 uploaded_file = st.file_uploader('Please upload your dog image', type='jpg')
 
 #load classifier
-model = load_model('model/20230930-Dogvision.h5')
+try:
+    model = load_model('model/20230930-Dogvision.h5')
+except Exception as e:
+    tempfolder = os.path.join(tempfile.gettempdir(), "tfhub_modules")
+    st.error(f'Error attempting to load model. Try clearing files in {tempfolder} and click re-run.')
+
 
 #display image
 with st.spinner("Loading...."):
     if uploaded_file != None:
-        image = Image.open(uploaded_file).convert('RGB')
-        st.image(image, use_column_width=True)
-
         #classify image
+        image = Image.open(uploaded_file).convert('RGB')
         class_name, conf_score = classify(image, model)
 
         # write classification
         st.write(f"## {class_name.title()}")
         st.progress(conf_score*0.01, text=f"### confidence {int(conf_score)}%")
+        st.image(image, use_column_width=True)
